02dd0f56ea6bee4cc87ab283fb2746746b612399
HANCKERS <cocshubh49@gmail.com>
2023-11-11 15:07:58 +0530

    done

--- Runnables/question1.cpp
+++ Runnables/question1.cpp
@@ -1,43 +1,79 @@
-#include "../Algorithms/Dijkstra/Dijkstra.h"  
-#include "../Helpers/Helpers.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 #include <ctime>
+
+#include "../../Helpers/Meta.h"
+#include "../../Helpers/Types.h"
+#include "../../Helpers/Timer.h"
+#include "../../Helpers/String/String.h"
+#include "../../Helpers/Vector/Vector.h"
+
+#include "../../DataStructures/Container/ExternalKHeap.h"
+#include "../../DataStructures/Container/Set.h"
+#include "../../DataStructures/Attributes/AttributeNames.h"
+
+#include "../Algorithms/Dijkstra/Dijkstra.h"
 
 using namespace std;
 
-int main() {
-    FloridaGraph floridaGraph;
-    // Load the graph from your data source (e.g., file or memory)
-
-    const int numPairs = 200;
-
-    srand(time(nullptr));
-
-    double totalRuntime = 0.0;
-
-    for (int i = 0; i < numPairs; ++i) {
-        const Vertex source = Random::nextInt(floridaGraph.numVertices());
-        const Vertex destination = Random::nextInt(floridaGraph.numVertices());
-
-        const auto start = chrono::high_resolution_clock::now();
-
-        Dijkstra<FloridaGraph> dijkstra(floridaGraph);
-        dijkstra.run(source, destination);
-
-        const auto end = chrono::high_resolution_clock::now();
-        const double runtime = chrono::duration<double>(end - start).count();
-
-        totalRuntime += runtime;
-
-        cout << "Source: " << source << ", Target: " << destination << "\n";
-        cout << "Shortest Path: ";
-        for (const Vertex vertex : dijkstra.getPath(destination)) {
-            cout << vertex << " - ";
-        }
-        cout << "\nShortest path length: " << dijkstra.getDistance(destination) << "\n\n";
+GRAPH readGraphFromFile(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error: Unable to open file " << filename << endl;
+        exit(EXIT_FAILURE);
     }
 
-    cout << "Total runtime in seconds for 200 random Dijkstra: " << totalRuntime << "\n";
+    GRAPH graph;
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+
+        if (line[0] == 'c') {
+            continue;  // Ignore comments
+        } else if (line[0] == 'p') {
+            string problemType;
+            int vertices, edges;
+            iss >> problemType >> vertices >> edges;
+            graph = GRAPH(vertices);
+        } else if (line[0] == 'a') {
+            int u, v, w;
+            iss >> u >> v >> w;
+            graph.addEdge(u, v, w);
+        }
+    }
+
+    file.close();
+    return graph;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));  // Seed for random number generation
+
+    GRAPH floridaGraph = readGraphFromFile("USA-road-d.FLA.gr");
+    Dijkstra<GRAPH> dijkstraInstance(floridaGraph);
+
+    clock_t start = clock();
+
+    const int numPairs = 200;
+    for (int i = 0; i < numPairs; ++i) {
+        int source = rand() % floridaGraph.vert();
+        int destination = rand() % floridaGraph.vert();
+
+        dijkstraInstance.run(source, destination);
+
+        cout << "Shortest Path: ";
+        auto path = dijkstraInstance.getPath(destination);
+        for (const auto& vertex : path) {
+            cout << vertex << ' ';
+        }
+        cout << "sp: " << dijkstraInstance.getDistance(destination) << endl;
+    }
+
+    clock_t endt = clock();
+    double runtime = static_cast<double>(endt - start) / CLOCKS_PER_SEC;
+    cout << "Total runtime in seconds for " << numPairs << " random Dijkstra: " << runtime << endl;
 
     return 0;
 }
